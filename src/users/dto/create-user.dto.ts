8edad62d300838d4c3581083577aa41af53d39d1
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2025-08-24 17:45:18 +0200

    Add role validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,10 @@
-import { IsEmail } from "class-validator";
+import { IsEmail, IsEnum } from "class-validator";
 export class CreateUserDto {
   name: string;
 
   @IsEmail()
   email: string;
+
+  @IsEnum(['ADMIN', 'INTERN', 'ENGINEER'], { message: 'Role must be ADMIN, INTERN or ENGINEER' })
   role: 'ADMIN' | 'INTERN' | 'ENGINEER';
 }
