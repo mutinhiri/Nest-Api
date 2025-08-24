c258882db8eecc3016235c65ff9ecf52e362b06c
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2025-08-24 17:23:51 +0200

    Extend partial type

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
 import { Param } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
