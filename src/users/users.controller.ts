c9030c5c5d5ce7e9a8250102a2aff7ab4f0a3c9b
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2025-08-27 16:03:14 +0200

    Parse integer type

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -26,7 +26,7 @@ export class UsersController {
         return this.usersService.create(user);
     }
 
-    @Patch(':id') //Update user
+    @Patch(':id') //Update user profile
     update(@Param('id', ParseIntPipe) id: number, @Body() userUpdate: {name: string, email: string, role: 'ADMIN' | 'INTERN' | "ENGINEER" }) {
         return this.usersService.update(id, userUpdate);
     }
